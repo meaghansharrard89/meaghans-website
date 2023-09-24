2d0417ce37d8d9339c28e08b7bc97046ceaf39a4
Meaghan <meaghan.sharrard89@gmail.com>
2023-09-23 21:42:52 -0700

    created survey form and collapsibles

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-const h2 = document.createElement("h2");
-h2.textContent = "Aspiring web developer with a passion for creative and functional design.";
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-let contactForm = document.getElementById("contactform");
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+//contact form
+let contactForm = document.getElementById("contactForm");
+
 contactForm.addEventListener("submit", (e) => {
     e.preventDefault();
-}
+
+    let firstname = document.getElementById("fname");
+    let lastname = document.getElementById("lname");
+    let email = document.getElementById("email");
+    let phone = document.getElementById("phone");
+    let subject = document.getElementById("subject");
+    let message = document.getElementById("message");
+
+    if (firstname.value == "" || lastname.value == "" || email.value == "" || message.value == "") {
+        console.log(alert("Please fill out all the fields!"));
+    } else {
+        console.log(alert("This form has been successfully submitted!"));
+    }
+});
