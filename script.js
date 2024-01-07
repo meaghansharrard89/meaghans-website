5b2ff8080bf9ca7f1ed09b42377febb041c48300
Meaghan <meaghan.sharrard89@gmail.com>
2024-01-07 15:38:04 -0800

    changed header font and added slow opening to collapsibles

--- script.js
+++ script.js
@@ -29,37 +29,66 @@ panelToActivate
 }*/
 
 //accordion
-var coll = document.getElementsByClassName("collapsible");
-var i;
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Wait for the DOM to be fully loaded before attaching event listeners
+  var collapsibles = document.querySelectorAll(".collapsible");
+
+  collapsibles.forEach(function (collapsible) {
+    collapsible.addEventListener("click", function () {
+      toggleAccordion(this);
+    });
   });
-}
+
+  function toggleAccordion(button) {
+    button.classList.toggle("active");
+    var content = button.nextElementSibling;
+
+    if (button.classList.contains("active")) {
+      // If the button is active, expand the content
+      content.style.maxHeight = content.scrollHeight + "px";
+    } else {
+      // If the button is not active, collapse the content
+      content.style.maxHeight = null;
+    }
+  }
+});
 
 //contact form
-let contactForm = document.getElementById("contactForm");
+// let contactForm = document.getElementById("contactForm");
 
-contactForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+// contactForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-    let firstname = document.getElementById("fname");
-    let lastname = document.getElementById("lname");
-    let email = document.getElementById("email");
-    let phone = document.getElementById("phone");
-    let subject = document.getElementById("subject");
-    let message = document.getElementById("message");
+//   let firstname = document.getElementById("fname");
+//   let lastname = document.getElementById("lname");
+//   let email = document.getElementById("email");
+//   let phone = document.getElementById("phone");
+//   let subject = document.getElementById("subject");
+//   let message = document.getElementById("message");
 
-    if (firstname.value == "" || lastname.value == "" || email.value == "" || message.value == "") {
-        console.log(alert("Please fill out all the fields!"));
-    } else {
-        console.log(alert("This form has been successfully submitted!"));
-    }
-});
+//   if (
+//     firstname.value == "" ||
+//     lastname.value == "" ||
+//     email.value == "" ||
+//     message.value == ""
+//   ) {
+//     console.log(alert("Please fill out all the fields!"));
+//   } else {
+//     console.log(alert("This form has been successfully submitted!"));
+//   }
+// });
